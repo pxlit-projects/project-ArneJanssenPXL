bab09e8c72c0bb99037d75dc5eb9a95bf5f4a465
ArneJanssenPXL <arne.janssen@student.pxl.be>
2024-11-13 11:00:48 +0100

    Create Backend Project

---
+++ backend-java/FullstackJavaPXL/src/test/java/be/pxl/services/fullstackjavapxl/FullstackJavaPxlApplicationTests.java
@@ -0,0 +1,13 @@
+package be.pxl.services.fullstackjavapxl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackJavaPxlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

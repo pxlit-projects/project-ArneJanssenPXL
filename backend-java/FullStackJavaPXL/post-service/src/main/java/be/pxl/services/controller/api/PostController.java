26a164607bff3dd0684d60ae7ad442de749964f2
ArneJanssenPXL <arne.janssen@student.pxl.be>
2024-11-17 15:10:07 +0100

    Add Logging - Config/Discovery/Gateway/Post

--- backend-java/FullStackJavaPXL/post-service/src/main/java/be/pxl/services/controller/api/PostController.java
+++ backend-java/FullStackJavaPXL/post-service/src/main/java/be/pxl/services/controller/api/PostController.java
@@ -3,6 +3,8 @@ package be.pxl.services.controller.api;
 import be.pxl.services.controller.request.PostRequest;
 import be.pxl.services.services.IPostService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,31 +14,37 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class PostController {
     private final IPostService postService;
+    private static final Logger log = LoggerFactory.getLogger(PostController.class);
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createPost(@RequestBody PostRequest postRequest){
+        log.info("Calling endpoint [POST] /api/post with params: {} in RequestBody", postRequest);
         postService.createPost(postRequest);
     }
 
     @GetMapping
     public ResponseEntity getAllPosts(){
+        log.info("Calling endpoint [GET] /api/post");
         return new ResponseEntity(postService.getAllPosts(), HttpStatus.OK);
     }
 
     @GetMapping("/concept")
     public ResponseEntity getAllConceptPosts(){
+        log.info("Calling endpoint [GET] /api/post/concept");
         return new ResponseEntity(postService.getConceptPosts(), HttpStatus.OK);
     }
 
     @GetMapping("/published")
     public ResponseEntity getAllPublishedPosts(){
+        log.info("Calling endpoint [GET] /api/post/published");
         return new ResponseEntity(postService.getPublishedPosts(), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void updatePost(@PathVariable Long id, @RequestBody PostRequest postRequest){
+        log.info("Calling endpoint [PUT] /api/post/{} with params: {} in PathVariable & with params: {} in RequestBody", id, id, postRequest);
         postService.updatePost(id, postRequest);
     }
 }
--- backend-java/FullStackJavaPXL/post-service/src/main/java/be/pxl/services/services/PostService.java
+++ backend-java/FullStackJavaPXL/post-service/src/main/java/be/pxl/services/services/PostService.java
@@ -6,6 +6,8 @@ import be.pxl.services.domain.Post;
 import be.pxl.services.exception.PostNotFoundException;
 import be.pxl.services.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PostService implements IPostService{
     private final PostRepository postRepository;
+    private static final Logger log = LoggerFactory.getLogger(PostService.class);
 
     private PostResponse mapToPostResponse(Post post){
         return PostResponse.builder()
@@ -27,6 +30,7 @@ public class PostService implements IPostService{
 
     @Override
     public PostResponse createPost(PostRequest postRequest) {
+        log.info("Creating post with post-request: {}", postRequest);
         Post post = Post.builder()
                 .id(postRequest.getId())
                 .author(postRequest.getAuthor())
@@ -35,40 +39,54 @@ public class PostService implements IPostService{
                 .isConcept(postRequest.isConcept())
                 .build();
 
+        log.info("Saving post ({}) in repository", post);
         post = postRepository.save(post);
+        log.info("Successfully saved post with id: {}", post.getId());
 
+        log.info("Mapping {} To PostResponse", post);
         return mapToPostResponse(post);
     }
 
     @Override
     public List<PostResponse> getAllPosts() {
+        log.info("Retrieving all posts");
         List<Post> posts = postRepository.findAll();
+        log.info("Mapping {} To PostResponse", posts);
         return posts.stream().map(this::mapToPostResponse).toList();
     }
 
     @Override
     public List<PostResponse> getConceptPosts() {
+        log.info("Retrieving all concept posts");
         List<Post> posts = postRepository.findByIsConcept(true);
+        log.info("Mapping {} To PostResponse", posts);
         return posts.stream().map(this::mapToPostResponse).toList();
     }
 
     @Override
     public List<PostResponse> getPublishedPosts() {
+        log.info("Retrieving all published posts");
         List<Post> posts = postRepository.findByIsConcept(false);
+        log.info("Mapping {} To PostResponse", posts);
         return posts.stream().map(this::mapToPostResponse).toList();
     }
 
     @Override
     public PostResponse updatePost(Long id, PostRequest postRequest) {
+        log.info("Finding post with id: {}", id);
         Post post = postRepository.findById(id).orElseThrow(() -> new PostNotFoundException("Post with id: " + id + " was not found."));
 
+        log.info("Updating post properties");
         post.setAuthor(postRequest.getAuthor());
         post.setContent(postRequest.getContent());
         post.setConcept(postRequest.isConcept());
         post.setDatePublished(postRequest.getDatePublished());
 
+        log.info("Saving post ({}) in repository", post);
         post = postRepository.save(post);
+        log.info("Successfully saved post with id: {}", post.getId());
 
+        log.info("Mapping {} To PostResponse", post);
         return mapToPostResponse(post);
     }
 }

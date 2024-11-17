df32863a94cb44567a406b6b4546b06d431c04e2
ArneJanssenPXL <arne.janssen@student.pxl.be>
2024-11-17 17:16:28 +0100

    Add Navbar and Welcome - With routing

--- frontend-web/FullstackJavaPXL/src/app/app.routes.ts
+++ frontend-web/FullstackJavaPXL/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: WelcomeComponent }, 
+    { path: 'home', component: WelcomeComponent }, 
+    { path: 'add-post', component: AddPostComponent }, 
+    { path: 'login', component: LoginComponent }
+];

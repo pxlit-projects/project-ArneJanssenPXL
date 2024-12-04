b5227a2e9ef199e310085e4b14cde852564caaef
ArneJanssenPXL <arne.janssen@student.pxl.be>
2024-12-04 11:37:19 +0100

    Add Angular Material Icon to NavBar

--- frontend-web/FullstackJavaPXL/src/app/navbar/navbar.component.ts
+++ frontend-web/FullstackJavaPXL/src/app/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../shared/services/auth.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
